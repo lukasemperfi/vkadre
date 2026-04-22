1cd3d779fef66d03fc002e4887392fa7b750452c
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-04-22 15:35:14 +0300

    set up styles and fonts

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  css: ['@/assets/styles/main.scss'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/styles/_functions.scss" as globalFunctions;
+            @use "@/assets/styles/_variables.scss" as globalVariables;
+            @use "@/assets/styles/_mixins.scss" as globalMixins;
+            @use "@/assets/styles/_breakpoints.scss" as globalBreakpoints;
+          `,
+        },
+      },
+    },
+  },
+})
