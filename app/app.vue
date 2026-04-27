4ee4cabd9ba29849a90cd3d02a7ce6c923012dd4
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-04-27 16:39:24 +0300

    create app layout

--- app/app.vue
+++ app/app.vue
@@ -58,7 +58,7 @@ const locationReviewsApi = useLocationReviewsApi();
 </script>
 
 <template>
-  <div>
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="app-layout">
+    <div>header</div>
+
+    <main class="main-content">
+      <slot />
+    </main>
+    <div>footer</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.main-content {
+  flex: 1;
+}
+</style>
